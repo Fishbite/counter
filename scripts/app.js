f7aadae77c9839fc981cb54769e224ca81abd8f9
Fishbite <43952420+Fishbite@users.noreply.github.com>
2021-04-04 19:09:31 +0100

    re-wrote js code after it got deleted :-P

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,26 @@
 // app code
+let count = 0;
+
+const value = document.getElementById("value"); // value to change on page
+const btns = document.querySelectorAll(".btn"); //create a node list which is array like
+
+//itterate over the node list of btns
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (event) {
+    const list = event.currentTarget.classList;
+    if (list.contains("decrease")) {
+      count--;
+    } else if (list.contains("increase")) {
+      count++;
+    } else if (list.contains("reset")) {
+      count = 0;
+    }
+    value.textContent = count;
+    if (count > 0) value.style.color = "green";
+    if (count < 0) value.style.color = "red";
+    if (count === 0) value.style.color = "black";
+    console.log(count);
+  });
+});
+
+//console.log("test....", btns);
